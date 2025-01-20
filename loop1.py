312d735f43c37c2711666738e4a7e46af55c5edd
Nyabayo <ernestosindo9@gmail.com>
2025-01-20 18:21:39 +0300

    loop1, loop2

---
+++ loop1.py
@@ -0,0 +1,7 @@
+# Write a program to list numbers from 1 to 10
+i = 1
+while i < 11:
+    print(i)
+    break    # break statement stops the loop even if the condition is true.
+    i += 1
+
---
+++ loop2.py
@@ -0,0 +1,7 @@
+i = 0
+while i <= 10:
+    i += 1
+    if i == 5:
+
+        continue  #  continue statement is used to stop the current iteration and continue with the next.
+    print(i)
